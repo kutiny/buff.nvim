655665d9fbd34900b149eff0f8e8a3b3794c74f8
kutiny <alex08lta@gmail.com>
2024-07-28 21:47:29 -0300

    feat: first rc

---
+++ lua/buff/autocmd.lua
@@ -0,0 +1,2 @@
+local augroup = vim.api.nvim_create_augroup("buffer.nvim", { clear = true })
+return augroup
--- lua/buff/init.lua
+++ lua/buff/init.lua
@@ -1,41 +1,69 @@
-local M = {}
+local Buff = {}
+local Buffer = require('buff.buffer')
+local Helpers = require('buff.helpers')
+local Config = require('buff.config')
+---@alias RuntimeGlobals {config: BuffConfig, buf: integer, win: integer, buffer_list: string[][] }
 
-local function get_home_dir()
-    local home = os.getenv("HOME")
-    if not home then
-        home = os.getenv("USERPROFILE")
+---@type RuntimeGlobals
+local Rt = {
+    config = Config.get_config({}),
+    buffer_list = {},
+}
+
+function Buff.toggle_buffer_list()
+    local current_buffer = vim.api.nvim_get_current_buf()
+
+    if Rt.buf == current_buffer then
+        vim.api.nvim_win_close(Rt.win, true)
+        return
     end
-    return home
-end
 
-function M.show_buffer_list()
+    Rt.buffer_list = {}
     local buffers = vim.api.nvim_list_bufs()
 
-    local text = {}
     for i = 1, #buffers do
         local is_active = vim.api.nvim_buf_is_loaded(buffers[i])
         if is_active then
             local buf_name = vim.api.nvim_buf_get_name(buffers[i])
-            local home_dir = get_home_dir()
-            local buf_text = string.gsub(buf_name, home_dir, '~')
-            table.insert(text, buf_text)
+
+            -- if buffer name is empty then ignore
+            if string.len(buf_name) == 0 then
+                goto continue
+            end
+
+            -- filter ignore patterns
+            if Rt.config.ignore_patterns then
+                for j = 1, #Rt.config.ignore_patterns do
+                    if string.match(buf_name, Rt.config.ignore_patterns[j]) then
+                        goto continue
+                    end
+                end
+            end
+
+            table.insert(Rt.buffer_list, {
+                buffers[i],
+                string.gsub(buf_name, tostring(Helpers.get_home_dir()), '~'),
+            })
         end
+        ::continue::
     end
 
-    local buf = vim.api.nvim_create_buf(false, true)
+    Rt.buf = vim.api.nvim_create_buf(false, true)
     local win_opts = {
-        relative = 'editor',
-        width = 80,
-        height = 10,
-        row = 1,
-        col = 0,
-        style = 'minimal',
-        border = 'single',
+        split = 'left',
+        win = -1,
+        width = 30,
     }
+    Rt.win = vim.api.nvim_open_win(Rt.buf, true, win_opts)
 
-    local win = vim.api.nvim_open_win(buf, true, win_opts)
-
-    vim.api.nvim_buf_set_lines(buf, 0, -1, false, text)
+    Buffer.initialize(Rt)
 end
 
-return M
+---@param user_config UserConfig
+function Buff.setup(user_config)
+    Rt.config = Config.get_config(user_config or {})
+end
+
+vim.api.nvim_create_user_command("BuffListToggle", Buff.toggle_buffer_list, {})
+
+return Buff
